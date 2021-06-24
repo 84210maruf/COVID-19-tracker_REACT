5937deabeb613e69cdb4f89d44a8d3af0cdab64e
maruf <maruf84210cse@gmail.com>
2021-06-24 19:22:16 +0600

    Adding Matrial UI Componants

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,9 +1,23 @@
+import {
+  FormControl, MenuItem, Select
+} from '@material-ui/core';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Hellow</h1>
+      <h1>COVID 19 TRACKER</h1>
+
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+        </Select>
+      </FormControl>
       
       {/* Header */}
       {/* Title + Select input */}
