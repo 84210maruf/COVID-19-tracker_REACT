109abaab9771c4b00f1eb607f81716272458b4d8
maruf <maruf84210cse@gmail.com>
2021-08-05 23:26:52 +0600

    Successfilly adding deaseas.sh API

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,7 +1,7 @@
 import {
   FormControl, MenuItem, Select
 } from '@material-ui/core';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 
@@ -9,12 +9,42 @@ import './App.css';
 function App() {
 
   
-  const [countries, setCountries] = useState(['usa','bd','india']);
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
   //COVID API
+  //https://disease.sh/v3/covid-19/countries
   //https://disease.sh/v3/covid-19/countries
   // useEffect = run a pice of code
   //based on given condition
+  useEffect(() => {
+      // async -> Sent a request, wait fo it, do somthing with it
+      //
+      const getCountriesData = async () => {
+        await fetch("https://corona.lmao.ninja/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          
+          const countries = data.map((country) => (
+            {
+              name: country.country,                    //india ,bangladesh,unitedStat
+              value: country.countryInfo.iso3,          //UK,USA,BD
+            }
+            ));
+            setCountries(countries);
+            // console.log(this);
+        });
   
+      };
+       getCountriesData();
+      
+    }, []);
+
+    // For SELECT Dropdown Item for target...
+
+    const onCountryChange = async (event) => {
+      const countryCode = event.target.value;
+      console.log("YOOOO >>>>", countryCode);
+    }
 
   
   return (
@@ -23,9 +53,10 @@ function App() {
       <div className="app__header">
         <h1>COVID 19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" onChange={onCountryChange} value={country} >
+          <MenuItem value="worldwide">worldwide</MenuItem>
             {countries.map((country) => (
-              <MenuItem value="{country}">{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
             
           </Select>
