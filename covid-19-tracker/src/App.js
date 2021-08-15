6e134659b16485c511a4eb6d91fc2b3ac40dd53b
maruf <maruf84210cse@gmail.com>
2021-08-15 22:59:26 +0600

    start warking with MAP

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,121 +1,164 @@
-import { Card, CardContent, FormControl, MenuItem, Select } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import InfoBox from './InfoBox';
-import LineGraph from './LineGraph';
-import Map from './Map';
-import Tables from './Tables';
-import { sortData } from './util';
+import {
+  Card,
+  CardContent,
+  FormControl,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import InfoBox from "./InfoBox";
+import LineGraph from "./LineGraph";
+import Map from "./Map";
+import Tables from "./Tables";
+import { sortData } from "./util";
 
 function App() {
-
-  
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
   const [tableData, setTableData] = useState([]);
 
-  // For Load the Worldwide cases
+  const [countrySlug, setCountrySlug] = useState("bangladesh");
+  const [coronaCountArr, setCoronaCountArr] = useState({});
+  const [coronaDateArr, setCoronaDateArr] = useState({});
+
+  const getCountryReportByDateRang = (CountrySlug, from, to) => {
+    console.log(countrySlug);
+    axios
+      .get(
+        `https://api.covid19api.com/country/${CountrySlug}/status/confirmed?from=${from}T00:00:00Z&to=${to}T00:00:00Z`
+      )
+      .then((res) => {
+        console.log("ByCountry", res);
+
+        const yAxisCoronaCount = res.data.map((d) => d.Cases);
+        const xAxisDateCount = res.data.map((d) => d.Date);
+        setCoronaCountArr(yAxisCoronaCount);
+        setCoronaDateArr(xAxisDateCount);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const formatDate = (date) => {
+    const d = new Date(date);
+    //2020-05-04;
+    const year = d.getFullYear();
+    const month = `0${d.getMonth() + 1}`.slice(-2); //12->012->12
+    const _date = d.getDate();
+    return `${year}-${month}-${_date}`;
+  };
+
+  //For Load the Worldwide cases
   useEffect(() => {
     fetch("https://corona.lmao.ninja/v3/covid-19/all")
-    .then((response) => response.json())
-    .then((data) => {
-      setCountryInfo(data);
-    });
-  },[]);
-  
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   useEffect(() => {
-      // async -> Sent a request, wait fo it, do somthing with it
-      const getCountriesData = async () => {
-        await fetch("https://corona.lmao.ninja/v3/covid-19/countries")
+    const getCountriesData = async () => {
+      await fetch("https://corona.lmao.ninja/v3/covid-19/countries")
         .then((response) => response.json())
         .then((data) => {
-          const countries = data.map((country) => (
-            {
-              name: country.country,                    //india ,bangladesh,unitedStat
-              value: country.countryInfo.iso3,          //UK,USA,BD
-            }
-            ));
-            const sortedData = sortData(data);
-            setTableData(sortedData);
-            setCountries(countries);
+          const countries = data.map((country) => ({
+            name: country.country, //india ,bangladesh,unitedStat
+            value: country.countryInfo.iso2, //UK,USA,BD
+          }));
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
         });
-  
-      };
-       getCountriesData();
-      
-    }, []);
+    };
+    getCountriesData();
+  }, []);
 
-    // For SELECT Dropdown Item for target...
-    // Also Use Tarnary Oparetor
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
     setCountry(countryCode);
-    // ternary working for change the link
-    const url = countryCode ==='worldwide' 
-    ? 'https://corona.lmao.ninja/v3/covid-19/all' 
-    : `https://corona.lmao.ninja/v3/covid-19/countries/${countryCode}`;
-    await fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      setCountry(countryCode);
-      // from the country response data
-      setCountryInfo(data);
-    })
-  }
-  console.log('Country Info',countryInfo);
 
-  
+    const url =
+      countryCode === "worldwide"
+        ? "https://corona.lmao.ninja/v3/covid-19/all"
+        : `https://corona.lmao.ninja/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("...........,,.,.,.,,: ", data);
+        setCountry(countryCode);
+
+        setCountryInfo(data);
+
+        // pREPAETRING gRAPH DATA
+        const d = new Date();
+        const to = formatDate(d);
+        const from = formatDate(d.setDate(d.getDate() - 7));
+
+        console.log("country : ", countrySlug + " DATE :", from, to);
+
+        getCountryReportByDateRang(countrySlug, from, to);
+      });
+  };
+
   return (
     <div className="app">
-     
       <div className="app__left">
-        {/* Header */}
-        {/* Title + Select input */}
         <div className="app__header">
           <h1>COVID 19 TRACKER</h1>
           <FormControl className="app__dropdown">
-            <Select variant="outlined" onChange={onCountryChange} value={country} >
-            <MenuItem value="worldwide">worldwide</MenuItem>
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">worldwide</MenuItem>
               {countries.map((country) => (
                 <MenuItem value={country.value}>{country.name}</MenuItem>
               ))}
             </Select>
           </FormControl>
         </div>
-        {/* InfoBoxes */}
+
         <div className="app__status">
-          <InfoBox 
-            title="Coronavirus Cases" 
-            cases={countryInfo.todayCases} 
-            total={countryInfo.cases} />
-          <InfoBox 
-            title="Recovered"  
-            cases={countryInfo.todayRecovered} 
-            total={countryInfo.recovered} />
-          <InfoBox 
-            title="Deaths"  
-            cases={countryInfo.todayDeaths} 
-            total={countryInfo.deaths}/>
+          <InfoBox
+            title="Coronavirus Cases"
+            cases={countryInfo.todayCases}
+            total={countryInfo.cases}
+          />
+          <InfoBox
+            title="Recovered"
+            cases={countryInfo.todayRecovered}
+            total={countryInfo.recovered}
+          />
+          <InfoBox
+            title="Deaths"
+            cases={countryInfo.todayDeaths}
+            total={countryInfo.deaths}
+          />
         </div>
-        {/* Map */}
-        <Map/>
+
+        <Map />
       </div>
 
-
-      <Card className="app__right">    
+      <Card className="app__right">
         <CardContent>
-          <h3>Live <strong className="caseColor">Case</strong> by Country..</h3>
-            <Tables countries={tableData}/>
+          <h3>
+            Live <strong className="caseColor">Case</strong> by Country..
+          </h3>
+          <Tables countries={tableData} />
 
-          <h3>World <strong className="caseColor">New</strong> Cases</h3>
+          <h3>
+            World <strong className="caseColor">New</strong> Cases
+          </h3>
           {/* Graph */}
-          <LineGraph/>
-          
+          <LineGraph yAxis={coronaCountArr} xAxis={coronaDateArr} />
         </CardContent>
       </Card>
-  </div>
+    </div>
   );
 }
 
--- covid-19-tracker/src/LineGraph.js
+++ covid-19-tracker/src/LineGraph.js
@@ -1,98 +1,39 @@
-import numeral from 'numeral';
-import React, { useEffect, useState } from 'react';
+// Another Graph
+
+import React from 'react';
 import { Line } from 'react-chartjs-2';
+import './Linegraph.css';
 
-function LineGraph({ casesType='cases'}) {
-    const [data, setData] = useState({});
-
-    const options = {
-        legend: {
-            display: false,
-        },
-        elements: {
-            point: {
-                redius: 0,
-            },
-        },
-        maintainAspectRatio: false,
-        tooltips: {
-            mode:"index",
-            intersect: false,
-            callbacks: {
-            label: function(tooltipItem, data) {
-                return numeral(tooltipItem.value).format("+0,0");
-                },
-            },
-        },
-        scales: {
-            xAxes: [
-                {
-                    type: "time",
-                    time: {
-                        format: "MM/DD/YY",
-                        tooltips: "ll",
-                    },
-                },
-            ],
-            yAxes: [
-                {
-                    gridLines: {
-                        display: false,
-                    },
-                    ticks: {
-                        //Include a dollewr sign in the ticks
-                        callcack: function (value, index, values) {
-                            return numeral(value).format("0a");
-                        },
-                    },
-                },
-            ],
-        },
-
-    };
-
-    const buildChartData = (data, casesType ='cases') => {
-        const chartData = [];
-        let lastDataPoint;
-        for(let date in data.cases) {
-            if(lastDataPoint) {
-                const newDataPoint = {
-                    x: date,
-                    y:data[casesType][date] - lastDataPoint,
-                };
-                chartData.push(newDataPoint);
-            }
-            lastDataPoint = data[casesType][data];
-        }
-        return chartData;
-    }
-
-    useEffect(() => {
-        const fetchData = async () => {
-            fetch('https://corona.lmao.ninja/v3/covid-19/historical/all?lastday=120')
-            .then(response => response.json())
-            .then(data => {
-                console.log('historical data for Graph',data);
-                const chartData = buildChartData(data, 'cases');
-                setData(chartData);
-            });
-        };
-        fetchData();
-    }, []);
-
+function LineGraph(props) {
     return (
-        <div>
-            <h2>I am a graph</h2>
-            {/*data is an object*/}
-            <Line 
-            options={options} 
-            data={{
-                datasets:[{
-                    backgroundColor:'rgba(204, 16, 52,0.5)',
-                    borderColor: '#CC1034',
-                    data: data
-                }]
-            }}/>
+        <div className="another_graph">
+            <Line data={{
+                
+                datasets: [
+                    {
+                    label: 'My First dataset',
+                    fill: true,
+                    lineTension: 0.1,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.1,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data:props.yAxis
+                    }
+                ]
+                }}
+            />
         </div>
     )
 }
